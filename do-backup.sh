f51066eb4fe4f6e6250a1dde196c08f1dfc40e1a
David Foerster <david.foerster@informatik.hu-berlin.de>
2017-07-08 12:35:29 +0200

    Back-up script

---
+++ do-backup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+readonly url='https://calendar.google.com/calendar/ical/irrlicht.verein%40gmail.com/public/basic.ics'
+#readonly ics=basic.ics
+readonly message='Regular calendar back-up'
+readonly author='Irrlicht e. V. <post@irrlicht-verein.de>'
+
+
+set -- -q --porcelain --no-allow-empty --no-edit --no-gpg-sign
+[ -z "${author-}" ] ||	set -- "$@" --author="$author"
+if [ -z "${ics-}" ]; then
+	ics="${url%%[\?\#]*}"
+	ics="${ics##*/}"
+	readonly ics
+fi
+
+
+if curl -z "$ics" -o "$ics" -- "$url" && [ -s "$ics" ]; then
+	sed -i -e '/^DTSTAMP:/d' -- "$ics"
+	if ! git diff --quiet HEAD -- "$ics"; then
+		git commit "$@" -om "$message" -- "$ics"
+		exec git push -q
+	fi
+else
+	git checkout -q -- "$ics"
+	exit 1
+fi
